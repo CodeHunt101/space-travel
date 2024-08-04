732266f7d05a2a06165e3e73730637a4da5526cb
Harold Torres <haroldtm55@gmail.com>
2024-08-04 17:02:13 +1000

    desktop view home page

--- src/app/components/LargeButton.tsx
+++ src/app/components/LargeButton.tsx
@@ -3,7 +3,7 @@ import style from './LargeButton.module.scss'
 const LargeButton = () => (
   <a
     href="#"
-    className={`${style['large-button']} uppercase ff-serif fs-600 text-dark bg-white`}
+    className={`${style['large-button']} uppercase ff-serif text-dark bg-white`}
   >
     <div>Explore</div>
   </a>

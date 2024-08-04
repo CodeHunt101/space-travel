feae67a3f9cf51580612a8d9e1c990a3fbce50d4
Harold Torres <haroldtm55@gmail.com>
2024-08-04 19:23:34 +1000

    add background for destination page

---
+++ src/app/destination/page.tsx
@@ -0,0 +1,11 @@
+import styles from './page.module.scss'
+
+const Destination = () => {
+  return (
+    <main
+      className={`${styles.destination} text-accent ff-serif text-accent`}
+    ></main>
+  )
+}
+
+export default Destination

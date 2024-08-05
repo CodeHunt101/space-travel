f4711a26e007890ae3ba7af9a46ed02b53e3fdb2
Harold Torres <haroldtm55@gmail.com>
2024-08-05 18:50:49 +1000

    refactor navbar and make it work

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,16 +1,33 @@
 'use client'
-import { useState } from 'react'
+import { useState, useCallback } from 'react'
 import styles from './NavBar.module.scss'
 import Image from 'next/image'
 import iconHamburger from '../../../public/assets/shared/icon-hamburger.svg'
 import iconClose from '../../../public/assets/shared/icon-close.svg'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import { Path } from '../utils/types'
 
 const NavBar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false)
+  const pathname = usePathname()
 
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen)
-  }
+  const toggleMenu = useCallback(() => {
+    setIsMenuOpen((prev) => !prev)
+  }, [])
+
+  const renderNavItem = (path: Path, label: string, index: string) => (
+    <li className={pathname === path ? 'active' : ''}>
+      <Link
+        className="uppercase text-white letter-spacing-2"
+        href={path}
+        aria-current={pathname === path ? 'page' : undefined}
+      >
+        <span>{index}</span>
+        {label}
+      </Link>
+    </li>
+  )
 
   return (
     <div
@@ -30,30 +47,10 @@ const NavBar = () => {
       </button>
       <nav className={styles.navbar}>
         <ul className={`underline-indicators ${isMenuOpen ? styles.open : ''}`}>
-          <li className="active">
-            <a
-              className="uppercase text-white letter-spacing-2"
-              href="#"
-              aria-current="page"
-            >
-              <span>00</span>Home
-            </a>
-          </li>
-          <li>
-            <a className="uppercase text-white letter-spacing-2" href="#">
-              <span>01</span>Destination
-            </a>
-          </li>
-          <li>
-            <a className="uppercase text-white letter-spacing-2" href="#">
-              <span>02</span>Crew
-            </a>
-          </li>
-          <li>
-            <a className="uppercase text-white letter-spacing-2" href="#">
-              <span>03</span>Technology
-            </a>
-          </li>
+          {renderNavItem(Path.HOME, 'Home', '00')}
+          {renderNavItem(Path.DESTINATION, 'Destination', '01')}
+          {renderNavItem(Path.CREW, 'Crew', '02')}
+          {renderNavItem(Path.TECHNOLOGY, 'Technology', '03')}
         </ul>
       </nav>
     </div>
---
+++ src/app/utils/types.ts
@@ -0,0 +1,6 @@
+export enum Path {
+  HOME = '/',
+  DESTINATION = '/destination',
+  CREW = '/crew',
+  TECHNOLOGY = '/technology',
+}

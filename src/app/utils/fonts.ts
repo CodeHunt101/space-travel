d2d84e1ce308dd8c50dfd79182c538dee0accbc7
Harold Torres <haroldtm55@gmail.com>
2024-08-07 18:52:49 +1000

    preload fonts

--- src/app/utils/fonts.ts
+++ src/app/utils/fonts.ts
@@ -5,12 +5,14 @@ const barlowCondensedInit = Barlow_Condensed({
   subsets: ['latin'],
   variable: '--ff-sans-cond',
   fallback: ['sans-serif'],
+  preload: true
 })
 const bellefairInit = Bellefair({
   weight: '400',
   subsets: ['latin'],
   variable: '--ff-serif',
   fallback: ['serif'],
+  preload: true
 })
 const barlowInit = Barlow({
   weight: ['400', '700'],
@@ -18,6 +20,7 @@ const barlowInit = Barlow({
   subsets: ['latin'],
   variable: '--ff-sans-normal',
   fallback: ['sans-serif'],
+  preload: true
 })
 
 export const barlowCondensed = barlowCondensedInit.variable

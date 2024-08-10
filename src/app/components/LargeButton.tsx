32056c4f44854207c75ea1b14f6edc3bddba4361
Harold Torres <haroldtm55@gmail.com>
2024-08-10 19:56:24 +1000

    add explore button path

--- src/app/components/LargeButton.tsx
+++ src/app/components/LargeButton.tsx
@@ -2,7 +2,7 @@ import style from './LargeButton.module.scss'
 
 const LargeButton = () => (
   <a
-    href="#"
+    href="/destination"
     className={`${style['large-button']} uppercase ff-serif text-dark bg-white`}
     role="button"
     aria-label="Explore more about space travel"
